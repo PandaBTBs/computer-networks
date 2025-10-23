f33dcc63f08c432d19869b694b9b4da6c01f0073
uy147 <118614536+PandaBTBs@users.noreply.github.com>
2025-10-23 15:25:28 +0300

    Add files via upload

---
+++ Server/Server.cpp
@@ -0,0 +1,157 @@
+// //ЗАДАЧА С ПАРЫ
+//#include <iostream>
+//#include <fstream>
+//#include <windows.h>
+//using namespace std;
+//struct Person
+//{
+//	char name[25];	 //имя 
+//	int height;		//рост
+//	int weight;		//вес
+//}B;
+//int answer;
+//long size_pred;
+//int main() {
+//	ifstream fR;
+//	ofstream fA;
+//	setlocale(LC_ALL, "rus");
+//	const char* nameR = "D:\REQUEST.bin";
+//	//файл запросов клиентов
+//	const char* nameA = "D:\ANSWER.bin";
+//	//файл ответов сервера
+//	cout << "server is working" << endl;
+//	// начальные установки
+//	fR.open(nameR, ios::binary);
+//	//открытие файла REQUEST
+//	fR.seekg(0, ios::end);
+//	size_pred = fR.tellg();
+//	//стартовая позиция сервера в файле REQUEST
+//	fR.close();
+//
+//	// начало работы сервера
+//	while (true)
+//	{
+//		fR.open(nameR, ios::binary);
+//		//открытие файла REQUEST
+//		fR.seekg(0, ios::end);
+//		//переход в конец файла REQUEST
+//		// есть новые запросы от клиентов? 
+//		while (size_pred >= fR.tellg())
+//		{
+//			Sleep(100); fR.seekg(0, ios::end);
+//		}
+//		// получен новый запрос 
+//		fR.seekg(size_pred, ios::beg);
+//		//переход к началу полученного запроса
+//		fR.read((char*)&B, sizeof(B));
+//		//считывание данных клиента
+//		size_pred = fR.tellg();
+//		// на конец обработанных данных
+//		fR.close();
+//		// определение индекса массы	
+//		double IMT = B.weight / (0.01 * B.height) / (0.01 * B.height);
+//		if (18.5 <= IMT && IMT < 25) answer = 1;
+//		if (18.5 > IMT) answer = 0;
+//		if (IMT >= 25)answer = 2;
+//
+//		// передача ответа клиенту
+//		fA.open(nameA, ios::binary | ios::app);
+//		//открытие файла ANSWER
+//		fA.write((char*)&answer, sizeof(answer));
+//		//запись ответа клиенту
+//		fA.close();
+//	}// while
+//}
+
+
+// server
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <windows.h>
+
+using namespace std;
+
+const string REQUEST_FILE = "D:\\REQUEST.bin";
+const string ANSWER_FILE = "D:\\ANSWER.bin";
+const int NAME_SIZE = 25;
+
+struct Student {
+    char lastName[NAME_SIZE];
+    int grades[4];
+};
+
+struct ScholarshipInfo {
+    int status;
+    int debtCount;
+};
+
+ScholarshipInfo analyzeGrades(const Student& student) {
+    ScholarshipInfo info;
+    info.status = 1;
+    info.debtCount = 0;
+
+    bool allExcellent = true;
+
+    for (int i = 0; i < 4; ++i) {
+        if (student.grades[i] < 4) {
+            info.status = 0;
+            info.debtCount++;
+            allExcellent = false;
+        }
+        else if (student.grades[i] < 5)
+        {
+            allExcellent = false;
+        }
+
+    }
+
+    if (allExcellent) {
+        info.status = 2;
+    }
+
+    return info;
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    cout << "Сервер запущен..." << endl;
+    cout << endl;
+
+    long lastFileSize = 0;
+
+    while (true) {
+        ifstream requestFile(REQUEST_FILE, ios::binary);
+        requestFile.seekg(0, ios::end);
+        long currentFileSize = requestFile.tellg();
+        requestFile.close();
+
+        if (currentFileSize > lastFileSize) {
+            // Обнаружен новый запрос
+            requestFile.open(REQUEST_FILE, ios::binary);
+            requestFile.seekg(lastFileSize, ios::beg);
+
+            Student student;
+            while (requestFile.read((char*)&student, sizeof(Student))) {
+                ScholarshipInfo scholarshipInfo = analyzeGrades(student);
+
+                ofstream answerFile(ANSWER_FILE, ios::binary | ios::app);
+                answerFile.write((char*)&scholarshipInfo, sizeof(ScholarshipInfo));
+                answerFile.close();
+
+                cout << "Обработан запрос для студента: " << student.lastName << endl;
+                cout << "Задолженности: " << scholarshipInfo.debtCount << endl;
+                cout << "Статус: " << scholarshipInfo.status << endl;
+                cout << endl;
+            }
+
+            lastFileSize = currentFileSize;
+            requestFile.close();
+        }
+        else {
+            Sleep(100);
+        }
+    }
+
+    return 0;
+}
